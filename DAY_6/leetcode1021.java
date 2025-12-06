65bf074552d50569f417e3a555c690d39cb7c358
ankit <ankitkushawaha4547@gmail.com>
2025-12-06 22:03:52 +0530

    leetcode new problem

---
+++ DAY_6/leetcode1021.java
@@ -0,0 +1,25 @@
+package DAY_6;
+
+public class leetcode1021 {
+    class Solution {
+    public String removeOuterParentheses(String s) {
+        
+        StringBuilder str = new StringBuilder();
+        int count =0;
+
+        for (int i=0; i< s.length(); i++){
+            if (s.charAt(i) == ')'){
+                count--;
+            }
+            if (count != 0 ){
+                str.append(s.charAt(i));
+            }
+            if (s.charAt(i) =='('){
+                count++;
+            }
+        }
+        return str.toString();
+    }
+}
+    
+}
