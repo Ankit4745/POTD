4243829256c60add069efe7c5db7e2ffa751dc86
ankit <ankitkushawaha4547@gmail.com>
2025-12-07 10:52:12 +0530

    leetcode new problem

---
+++ DAY_7/leetcode560.java
@@ -0,0 +1,19 @@
+package DAY_7;
+
+public class leetcode560 {
+    class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count =0;
+        for (int i=0; i< nums.length; i++){
+            int currS=0;
+            for (int j=i; j<nums.length; j++){
+                currS += nums[j];
+                if (currS == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+}
