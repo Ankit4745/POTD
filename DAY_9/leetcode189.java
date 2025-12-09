07c0e94437a1c4861fb6b143ecde2604031b9174
ankit <ankitkushawaha4547@gmail.com>
2025-12-09 12:51:36 +0530

    leetcode   new problem

---
+++ DAY_9/leetcode189.java
@@ -0,0 +1,18 @@
+package DAY_9;
+
+public class leetcode189 {
+    class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        k = k % nums.length;
+        for (int i=0; i<nums.length; i++){
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int j=0 ; j < nums.length; j++){
+            nums[j] = a [j];
+        }
+        
+    }
+}
+    
+}
