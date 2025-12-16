9b37ecdf9cebde2554377de8a685810f5faf4218
ankit <ankitkushawaha4547@gmail.com>
2025-12-16 10:17:01 +0530

    codeforce new problem of rating 1200

---
+++ DAY_15/codeforces514A.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class codeforces514A {
+      public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+       char[] arr = s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            int digit = arr[i] -'0';
+            int temp = 9-digit;
+            if (i==0 && temp == 0) {
+                continue;
+            }
+            if (temp < digit){
+                arr[i] = (char)(temp + '0');
+            }
+        }
+        System.out.println(new String(arr));
+
+    }
+        }
+
