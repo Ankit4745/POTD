d8f95852ed91e02e137da240752f5f115e6152a8
ankit <ankitkushawaha4547@gmail.com>
2026-01-07 00:09:14 +0530

    leetcode problem 2593

---
+++ DAY_36/leetcode191.java
@@ -0,0 +1,15 @@
+package DAY_36;
+
+public class leetcode191 {
+
+    class Solution {
+    public int hammingWeight(int n) {
+        int count =0;
+        while (n>0){
+            count += n % 2;
+            n /=2;
+        }
+        return count;
+    }
+}
+}
