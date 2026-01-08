632b1edf0e35b8957984db47b3f08ccbb763b677
ankit <ankitkushawaha4547@gmail.com>
2026-01-08 19:25:17 +0530

    codeforces problem 579A

---
+++ DAY_37/codeforces579A.java
@@ -0,0 +1,20 @@
+package DAY_37;
+ import java.util.Scanner;
+public class codeforces579A {
+   
+
+
+
+     public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   int test = sc.nextInt();
+    String s = Integer.toBinaryString(test);
+    int count = 0;
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i) == '1') count++;
+    }
+    System.out.println(count);
+   
+     }
+
+}
