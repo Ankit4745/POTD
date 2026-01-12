54cb2bed3557bcfc8a7d06b4ae3f7318674520ca
ankit <ankitkushawaha4547@gmail.com>
2026-01-12 08:48:43 +0530

    leetcode problem 344

---
+++ DAY_40/leetcode344.java
@@ -0,0 +1,25 @@
+package DAY_40;
+
+import java.util.Stack;
+
+public class leetcode344 {
+    class Solution {
+    public void reverseString(char[] s) {
+
+
+       
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0;i<s.length;i++){
+            stack.push(s[i]);
+        }
+        // int j=0;
+        int j=0;
+        while(!stack.isEmpty()){
+            s[j++] = stack.pop();
+        }
+   
+    }
+}
+        
+
+}
