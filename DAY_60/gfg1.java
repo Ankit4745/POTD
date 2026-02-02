43e02f7aed38202e5cdb1a256813023783d7aade
ankit <ankitkushawaha4547@gmail.com>
2026-02-02 21:22:20 +0530

    question of gfg

---
+++ DAY_60/gfg1.java
@@ -0,0 +1,10 @@
+public class gfg1 {
+    class Solution {
+    static void printTillN(int N) {
+        // code here
+        for(int i=1;i<=N;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
+}
